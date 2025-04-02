5a90b5eb84fdb548ed2679785a4e09cdb6751eec
Yerraguntla Venkata Sai <saivenkatt942@gmail.com>
2025-04-02 17:36:36 +0530

    postcss and tailwind conflicts resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: "class",
   content: [
     './pages/**/*.{ts,tsx}',
@@ -18,7 +18,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        black: '#000000',
+        black: "#000000",
+        white: "#ffffff",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
